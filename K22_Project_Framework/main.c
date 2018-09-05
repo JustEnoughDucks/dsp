dd9999f36ade3459e95377ee5211bfc9db3ecf38
Ben <bpgerbe@gmail.com>
2018-09-05 12:08:03 -0500

    DSP Project framework w/ part of HW1 done

---
+++ K22_Project_Framework/main.c
@@ -0,0 +1,37 @@
+/**************************************************************************/
+//Name:  main.c																														//
+//Purpose:  Skeleton project with configuration for ADC, DAC, MCG and PIT	//
+//Author:  Ethan Hettwer																									//
+//Revision:  1.0 15Sept2014 EH Initial Revision														//
+//Target:  Freescale K22f																									//
+/**************************************************************************/
+
+#include "MK22F51212.h"                 				//Device header
+#include "MCG.h"																//Clock header
+#include "TimerInt.h"														//Timer Interrupt Header
+#include "ADC.h"																//ADC Header
+#include "DAC.h"																//DAC Header
+
+uint16_t valADC;
+
+void PIT0_IRQHandler(void){	//This function is called when the timer interrupt expires
+	//Place Interrupt Service Routine Here
+	valADC = ADC0->R[0];
+	ADC_SC2_ADTRG(0);
+	
+	DAC0->DAT[8] = ;
+	
+	NVIC_ClearPendingIRQ(PIT0_IRQn);							//Clears interrupt flag in NVIC Register
+	PIT->CHANNEL[0].TFLG	= PIT_TFLG_TIF_MASK;		//Clears interrupt flag in PIT Register
+}
+
+int main(void){
+	MCG_Clock120_Init();
+	ADC_Init();
+	ADC_Calibrate();
+	DAC_Init();
+	TimerInt_Init();
+	while(1){
+		//Main loop goes here
+	}
+}

11029192d926cf25afd60c8add33066b7e5267f4
Ben <bpgerbe@gmail.com>
2018-09-06 16:10:04 -0500

    Finished Homework 1

--- K22_Project_Framework/main.c
+++ K22_Project_Framework/main.c
@@ -14,17 +14,32 @@
 
 uint16_t valADC;
 uint16_t valDAC;
+int i = 1, direction = 1;
+
 
 void PIT0_IRQHandler(void){	//This function is called when the timer interrupt expires
 	//Place Interrupt Service Routine Here
-	valADC = ADC0->R[0];
+	valADC = ADC0->R[0];// << 4;
 	ADC0->SC1[0]	=	ADC_SC1_ADCH(0x00);
-	//ADC_SC2_ADTRG(0);
+	if(direction)
+	{
+		i++;
+		if(i == 20000)
+		{
+			direction = 0;
+		}
+	} else if(!direction)
+	{
+		if( i == 0)
+		{
+			direction = 1;
+		}
+		i--;
+	}
+	valDAC = valADC * (i+2000)/22000;
 	
-	valDAC = valADC;
-	
-	DAC0->DAT->DATL = DAC_DATL_DATA0((valDAC >> 4)	&0xFF)	;		//Set DAC Output
-	DAC0->DAT->DATH = DAC_DATH_DATA1((valDAC >> 12)	&0x0F)	;		//Set DAC Output
+	DAC0->DAT->DATL = DAC_DATL_DATA0((valDAC))	;		//Set DAC Output
+	DAC0->DAT->DATH = DAC_DATH_DATA1((valDAC >> 8)	&0x0F)	;		//Set DAC Output
 	
 	NVIC_ClearPendingIRQ(PIT0_IRQn);							//Clears interrupt flag in NVIC Register
 	PIT->CHANNEL[0].TFLG	= PIT_TFLG_TIF_MASK;		//Clears interrupt flag in PIT Register

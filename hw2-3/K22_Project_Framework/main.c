cedf0dd347abee6ef8e761aba86524bee8ecac75
Ben Gerber <benjamin.gerber@ndsu.edu>
2018-09-18 15:22:03 -0500

    working on hw problem 3

--- hw2-3/K22_Project_Framework/main.c
+++ hw2-3/K22_Project_Framework/main.c
@@ -11,35 +11,33 @@
 #include "TimerInt.h"														//Timer Interrupt Header
 #include "ADC.h"																//ADC Header
 #include "DAC.h"																//DAC Header
+#include "math.h"																//Math Header
 
-uint16_t valADC;
-uint16_t valDAC;
-int i = 1, direction = 1;
+uint16_t valADC, valDAC;
+uint8_t highMask, lowMask;
+int N = 0, i;
 
 
 void PIT0_IRQHandler(void){	//This function is called when the timer interrupt expires
 	//Place Interrupt Service Routine Here
 	valADC = ADC0->R[0];// << 4;
 	ADC0->SC1[0]	=	ADC_SC1_ADCH(0x00);
-	if(direction)
-	{
-		i++;
-		if(i == 2000)
-		{
-			direction = 0;
-		}
-	} else if(!direction)
-	{
-		if( i == 0)
-		{
-			direction = 1;
-		}
-		i--;
-	}
-	valDAC = valADC * (i+200)/2200;
+
 	
-	DAC0->DAT->DATL = DAC_DATL_DATA0((valDAC))	;		//Set DAC Output
-	DAC0->DAT->DATH = DAC_DATH_DATA1((valDAC >> 8)	&0x0F)	;		//Set DAC Output
+	if(N <= 8)
+	{
+		highMask = 0x00;
+		lowMask = (0xFF >> (8 - N)); 
+	}else if(N > 8 && N < 13)
+	{
+		lowMask = 0xFF;
+		highMask = (0x0F >> (12-N));
+	}
+	
+	valDAC = valADC;
+	
+	DAC0->DAT->DATL = DAC_DATL_DATA0((valDAC) &lowMask)	;		//Set DAC Output
+	DAC0->DAT->DATH = DAC_DATH_DATA1((valDAC >> 8)	&highMask)	;		//Set DAC Output
 	
 	NVIC_ClearPendingIRQ(PIT0_IRQn);							//Clears interrupt flag in NVIC Register
 	PIT->CHANNEL[0].TFLG	= PIT_TFLG_TIF_MASK;		//Clears interrupt flag in PIT Register

cb7ef4a856a35ba0dc0396acdff290235c055abf
Ben Gerber <benjamin.gerber@ndsu.edu>
2018-11-07 19:36:19 -0600

    finished code bittons and interrupt (unoptimized)

--- hw6/K22_Project_Framework/main.c
+++ hw6/K22_Project_Framework/main.c
@@ -15,14 +15,20 @@
 #include "coef.h"
 
 //#define PI 3.14159
-#define sw2 !((GPIOB->PDIR >> 1) & 1)
-#define sw3 !((GPIOC->PDIR >> 17) &1)
+#define SW2 !((GPIOC->PDIR >> 1) & 1)
+#define SW3 !((GPIOB->PDIR >> 17) &1)
+
+#define MAXIMUM 1024
 
 uint16_t valADC;
 uint16_t valDAC;
-uint16_t i, k;
+uint16_t k;
+uint8_t scroll = 0;
+uint16_t integrator1 = 0;
+uint16_t integrator2 = 0;  /* Will range from 0 to the specified MAXIMUM */
+uint16_t outputBut;      /* Cleaned-up version of the input signal */
 
-float Yn[Korder + 1] = {0};
+float Yn[Korder+1] = {0};
 float s[Korder][2] = {{0}};
 
 void PIT0_IRQHandler(void){	//This function is called when the timer interrupt expires
@@ -35,25 +41,61 @@ void PIT0_IRQHandler(void){	//This function is called when the timer interrupt e
 
 	Yn[0] = (float) valADC;
 
-	for(i = 0; i < Korder; i++)
+	switch(scroll)
 	{
-			Yn[i+1] = B[i][0]*Yn[i] + s[i][0];
-      s[i][0] = B[i][1]*Yn[i] - A[i][0]*Yn[i+1] + s[i][1];
-      s[i][1] = B[i][0]*Yn[i] - A[i][1]*Yn[i+1];
+		case 0: {
+			Yn[Korder] = (float) valADC;
+			break;
+		}
+		case 1: {
+			for(int i = 0; i < Korder; i++)
+			{
+					Yn[i+1] = B1[i][0]*Yn[i] + s[i][0];
+					s[i][0] = B1[i][1]*Yn[i] - A1[i][0]*Yn[i+1] + s[i][1];
+					s[i][1] = B1[i][0]*Yn[i] - A1[i][1]*Yn[i+1];
+			}
+			break;
+		};
+		case 2: {
+			for(int i = 0; i < Korder; i++)
+			{
+					Yn[i+1] = B2[i][0]*Yn[i] + s[i][0];
+					s[i][0] = B2[i][1]*Yn[i] - A2[i][0]*Yn[i+1] + s[i][1];
+					s[i][1] = B2[i][0]*Yn[i] - A2[i][1]*Yn[i+1];
+			}
+			break;
+		};
+		case 3: {
+			for(int i = 0; i < Korder; i++)
+			{
+					Yn[i+1] = B3[i][0]*Yn[i] + s[i][0];
+					s[i][0] = B3[i][1]*Yn[i] - A3[i][0]*Yn[i+1] + s[i][1];
+					s[i][1] = B3[i][0]*Yn[i] - A3[i][1]*Yn[i+1];
+			}
+			break;
+		}
+		case 4: {
+			for(int i = 0; i < Korder; i++)
+			{
+					Yn[i+1] = B4[i][0]*Yn[i] + s[i][0];
+					s[i][0] = B4[i][1]*Yn[i] - A4[i][0]*Yn[i+1] + s[i][1];
+					s[i][1] = B4[i][0]*Yn[i] - A4[i][1]*Yn[i+1];
+			}
+			break;
+		}
 	}
-
 	valDAC = (uint16_t) (Yn[Korder]);
+
 
 	
 	DAC0->DAT->DATH = DAC_DATH_DATA1((valDAC >> 8)	&0x0F)	;		//Set DAC Output
 	DAC0->DAT->DATL = DAC_DATL_DATA0((valDAC))	;		//Set DAC Output
-
-	GPIOA->PSOR = GPIO_PSOR_PTSO(0x0006);		//Turn on Red LED
-	GPIOD->PSOR = GPIO_PSOR_PTSO(0x0020);		//Turn Blue On
 	
 	NVIC_ClearPendingIRQ(PIT0_IRQn);							//Clears interrupt flag in NVIC Register
 	PIT->CHANNEL[0].TFLG	= PIT_TFLG_TIF_MASK;		//Clears interrupt flag in PIT Register
 
+	GPIOA->PSOR = GPIO_PSOR_PTSO(0x0006);		//Turn on Red LED
+	GPIOD->PSOR = GPIO_PSOR_PTSO(0x0020);		//Turn Blue On
 }
 
 int main(void){
@@ -63,27 +105,98 @@ int main(void){
 	DAC_Init();
 	TimerInt_Init();
 	
-	//Set Port GPIO
-	SIM -> SCGC5 = 0x1E00;
-	
-	//Set LED Mode to AT1
-	PORTA->PCR[1] 	= PORT_PCR_MUX(0x01)					;		//Set PTA1 to ALT1
-	PORTA->PCR[2]		= PORT_PCR_MUX(0x01)					;		//
-	PORTB->PCR[17]	= PORT_PCR_MUX(0x01)					;		//
-	PORTC->PCR[1]		=	PORT_PCR_MUX(0x01)					;		//
-	PORTD->PCR[5] 	= PORT_PCR_MUX(0x01)					;		//Set PTD5 to ALT1
-	
-	//Set LED to Output
-	GPIOA -> PDDR = GPIO_PDDR_PDD(0x0006);
-	GPIOD -> PDDR = GPIO_PDDR_PDD(0x0020); 
-	
-	//Set Switch inputs
-	GPIOB -> PDDR = GPIO_PDDR_PDD(0x0000); //Set PORTB (SW3) as input
-	GPIOC -> PDDR = GPIO_PDDR_PDD(0x0000); //Set PORTC (SW2) as input
+	//Enable clock to ports through SCGC5 (for buttons and LEDs)
+	SIM->SCGC5 = SIM_SCGC5_PORTA(1)|SIM_SCGC5_PORTB(1)|SIM_SCGC5_PORTC(1)|SIM_SCGC5_PORTD(1);
 
-	int myVariableThatIsNeverUsed = 0;
+	//set the corresponding port Pin Control Register to configure the MUX field so that the pin is configured GPIO
+	PORTA->PCR[1] = PORT_PCR_MUX(001);//set PTA1 to ALT1 (Red LED)
+	PORTA->PCR[2] = PORT_PCR_MUX(001);//set PTA2 to ALT1 (Green LED)
+	PORTD->PCR[5] = PORT_PCR_MUX(001);//set PTD5 to ALT1 (Blue LED)
+	PORTC->PCR[1]= PORT_PCR_MUX(001);//set PTC1 to ALT1 (SW2)
+	PORTB->PCR[17] = PORT_PCR_MUX(001);//set PTB17 to ALT1 (SW3)
+	GPIOB->PDDR=0x0000;//set "port data direction register" to input (SW3)
+	GPIOC->PDDR=0x0000;//set "port data direction register" to input (SW2)
+	GPIOA->PDDR=0x0006;//set LED at ports A1 and A2 to output(Red & Green LEDs)
+	GPIOD->PDDR=0x0020;//set led at port D5 to output(Blue LED)
+
+	//int myVariableThatIsNeverUsed = 0;
+	int p = 0;
 	while(1){
 		//Main loop goes here
+		//may need to implement a software debounce to realize reliable pushbutton operation 
+		
+		/*if (SW2&&p!=2)
+			{
+				scroll++;
+				p = 2;
+			}		
+			if (SW3&&p!=3)
+			{
+				scroll--;
+				p = 3;
+			}
+			if (!SW2&&!SW3)
+			{
+			 p = 0;
+			}
+			if (scroll>4)
+			{
+				scroll=0;
+			}	*/
+		
+		//Step 1: Update the integrator based on the input signal.  Note that the
+		//integrator follows the input, decreasing or increasing towards the limits as
+		//determined by the input state (0 or 1). 
+	 
+		if (!SW2)
+		{
+			if (integrator1 > 0)
+			{
+				integrator1--;
+			}
+		}
+		else if (SW2 && integrator1 < MAXIMUM)
+		{
+			integrator1++;
+		}
+		
+		if (!SW3)
+		{
+			if (integrator2 > 0)
+			{
+				integrator2--;
+			}
+		}
+		else if (SW3 && integrator2 < MAXIMUM)
+		{
+			integrator2++;
+		}
+	 
+		//Step 2: Update the output state based on the integrator.  Note that the
+		//output will only change states if the integrator has reached a limit, either
+		//0 or MAXIMUM.
+	 
+		if (integrator1 >= MAXIMUM && scroll > 4)
+		{
+			scroll = 0;
+		}
+		else if (integrator1 >= MAXIMUM && p != 2)
+		{
+			scroll++;
+			integrator1 = MAXIMUM - 25;  //defensive code if integrator got corrupted
+			p = 2;
+		}
+		if (integrator2 >= MAXIMUM && scroll > 0 && p != 3)
+		{
+			scroll--;
+			integrator2 = MAXIMUM - 25;  //defensive code if integrator got corrupted
+			p = 3;
+		}
+		if( integrator1 == 0 && integrator2 == 0)
+		{
+			p = 0;
+		}
 		
 	}
 }
+

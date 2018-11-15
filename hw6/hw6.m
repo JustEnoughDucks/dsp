bb27d7a91f936bf00a958486dc1da9cf89eb71fd
Ben <bpgerbe@gmail.com>
2018-11-15 10:05:50 -0600

    changed mat files to correct format

--- hw6/hw6.m
+++ hw6/hw6.m
@@ -88,20 +88,20 @@ for fp = [250 500 1000 2000]
         [imag(pzSort(i,1)) imag(pzSort(i, 3))], 'color', [1/(K+.1*K)*(i) 1/(K+.1*K)*i 1/(K+.1*K)*i]);
     end
 
-    A(K,3) = 0;
-    B(K,3) = 0;
+    Acoef(K,3) = 0;
+    Bcoef(K,3) = 0;
     for i = 1:K
-        A(i,:) = poly(pzSort(i,[1 2]));
-        B(i,:) = poly(pzSort(i,[3 4]));
+        Acoef(i,:) = poly(pzSort(i,[1 2]));
+        Bcoef(i,:) = poly(pzSort(i,[3 4]));
     end
 
     gain = single(nthroot(real((bL/aK*prod(c2-zi)/prod(c2-pk))),K));
-    B = single(gain*B);
-    A = single(A);
-    B = flipud(B);
-    A = flipud(A);
-    A(:,1) = [];
-    B(:,3) = [];
+    Bcoef = single(gain*Bcoef);
+    Acoef = single(Acoef);
+    Bcoef = flipud(Bcoef);
+    Acoef = flipud(Acoef);
+    Acoef(:,1) = [];
+    Bcoef(:,3) = [];
 
     if fp == 250
         fid = fopen('K22_Project_Framework/coef.h','w');
@@ -111,15 +111,15 @@ for fp = [250 500 1000 2000]
     end
         fprintf(fid,'float A%i[Korder][2] = { \n',z);
         for Korder = 1:K-1
-            fprintf(fid,'{%f, %f} ,\n', A(Korder,1), A(Korder,2));
+            fprintf(fid,'{%f, %f} ,\n', Acoef(Korder,1), Acoef(Korder,2));
         end
-        fprintf(fid,'{%f, %f} \n};\n\n', A(K,1), A(K,2));
+        fprintf(fid,'{%f, %f} \n};\n\n', Acoef(K,1), Acoef(K,2));
 
         fprintf(fid,'float B%i[Korder][2] = { \n',z);
         for Korder = 1:K-1
-            fprintf(fid,'{%f, %f} ,\n', B(Korder,1), B(Korder,2));
+            fprintf(fid,'{%f, %f} ,\n', Bcoef(Korder,1), Bcoef(Korder,2));
         end
-        fprintf(fid,'{%f, %f} \n};\n\n', B(K,1), B(K,2));
+        fprintf(fid,'{%f, %f} \n};\n\n', Bcoef(K,1), Bcoef(K,2));
         fclose(fid);
         
 
@@ -140,9 +140,9 @@ for fp = [250 500 1000 2000]
         Yn(1,1) = 2048*sin(500*2*pi*t) + 2047;
 
         for k = 1:K
-                Yn(k+1) = B(k,1)*Yn(k) + s(k,1);
-                s(k,1) = B(k,2)*Yn(k) - A(k,1)*Yn(k+1) + s(k,2);
-                s(k,2) = B(k,1)*Yn(k) - A(k,2)*Yn(k+1);
+                Yn(k+1) = Bcoef(k,1)*Yn(k) + s(k,1);
+                s(k,1) = Bcoef(k,2)*Yn(k) - Acoef(k,1)*Yn(k+1) + s(k,2);
+                s(k,2) = Bcoef(k,1)*Yn(k) - Acoef(k,2)*Yn(k+1);
         end
         valDAC = Yn(K+1);
 
@@ -157,8 +157,8 @@ for fp = [250 500 1000 2000]
     figure;
     Hz = @(z) 1;
     for k = 1:K
-        Hz = @(z) Hz(z).*(B(k,1)*z.^2 + B(k,2).*z + B(k,1)) ...
-            ./(1*z.^2 + A(k,1).*z + A(k,2));
+        Hz = @(z) Hz(z).*(Bcoef(k,1)*z.^2 + Bcoef(k,2).*z + Bcoef(k,1)) ...
+            ./(1*z.^2 + Acoef(k,1).*z + Acoef(k,2));
 
     end
 
@@ -173,11 +173,10 @@ for fp = [250 500 1000 2000]
     % Wrong way to implement it. It shoud be Arrary(columns, rows, page)
     % but is specified as Array(page, row, column)
     
-    OnesArray(length(A(:,1)),1) = 1;
-    matArrayA(z,:,:) = [OnesArray(:,1),A(:,1),A(:,2)];
-    matArrayB(z,:,:) = [B(:,1), B(:,2), B(:,1)];
+    OnesArray(length(Acoef(:,1)),1) = 1;
+    A(z,:,:) = [OnesArray(:,1),Acoef(:,1),Acoef(:,2)];
+    B(z,:,:) = [Bcoef(:,1), Bcoef(:,2), Bcoef(:,1)];
 
 end
 
-save('arrayA.mat','matArrayA');
-save('arrayB.mat','matArrayB');
+save('bpg.mat','A','B');

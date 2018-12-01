f65169957b2d95addb798f9c308e63fe478e9bb5
bpgerber <bpgerbe@gmail.com>
2018-12-01 11:46:41 -0600

    Finished hw7 matlab, and starting C code testing

---
+++ hw7/K22_Project_Framework/main.c
@@ -0,0 +1,171 @@
+/**************************************************************************/
+//Name:  main.c																														//
+//Purpose:  Skeleton project with configuration for ADC, DAC, MCG and PIT	//
+//Author:  Ben Gerber																											//
+//Revision:  1.0 18Oct2018 BG DSP Chebyshev Type II												//
+//Target:  Freescale K22f																									//
+/**************************************************************************/
+
+#include "MK22F51212.h"                 				//Device header
+#include "MCG.h"																//Clock header
+#include "TimerInt.h"														//Timer Interrupt Header
+#include "ADC.h"																//ADC Header
+#include "DAC.h"																//DAC Header
+#include "math.h"																//Math Header
+#include "coef.h"
+
+//#define PI 3.14159
+#define SW2 !((GPIOC->PDIR >> 1) & 1)
+#define SW3 !((GPIOB->PDIR >> 17) &1)
+
+#define MAXIMUM 1024
+
+uint16_t valADC;
+uint16_t valDAC;
+uint8_t scroll = 0;
+uint16_t integrator1 = 0;
+uint16_t integrator2 = 0;  /* Will range from 0 to the specified MAXIMUM */
+uint16_t outputBut;      /* Cleaned-up version of the input signal */
+
+int Y;
+int s[Korder] = {0};
+
+void PIT0_IRQHandler(void){	//This function is called when the timer interrupt expires
+	//Place Interrupt Service Routine Here
+	GPIOA->PCOR = GPIO_PCOR_PTCO(0x0006);		//Turn Red Off
+	GPIOD->PCOR = GPIO_PCOR_PTCO(0x0020);		//Turn off Blue LED
+	
+	valADC = ADC0->R[0];
+	ADC0->SC1[0]	=	ADC_SC1_ADCH(0x00);
+
+	switch(scroll)
+	{
+		case 0: {
+			Yn[Korder] = (uint16) valADC;
+			break;
+		}
+		case 1: {
+			for(int i = 0; i < Korder; i++)
+			{
+					Y = h[i]*ValADC + s[i];
+					s[i] = h[i]*ValADC ;
+			}
+			break;
+		};
+	}
+	valDAC = (uint16_t) (Yn);
+
+
+	
+	DAC0->DAT->DATH = DAC_DATH_DATA1((valDAC >> 8)	&0x0F)	;		//Set DAC Output
+	DAC0->DAT->DATL = DAC_DATL_DATA0((valDAC))	;		//Set DAC Output
+	
+	NVIC_ClearPendingIRQ(PIT0_IRQn);							//Clears interrupt flag in NVIC Register
+	PIT->CHANNEL[0].TFLG	= PIT_TFLG_TIF_MASK;		//Clears interrupt flag in PIT Register
+
+	GPIOA->PSOR = GPIO_PSOR_PTSO(0x0006);		//Turn on Red LED
+	GPIOD->PSOR = GPIO_PSOR_PTSO(0x0020);		//Turn Blue On
+}
+
+int main(void){
+	MCG_Clock120_Init();
+	ADC_Init();
+	ADC_Calibrate();
+	DAC_Init();
+	TimerInt_Init();
+	
+	//Enable clock to ports through SCGC5 (for buttons and LEDs)
+	SIM->SCGC5 = SIM_SCGC5_PORTA(1)|SIM_SCGC5_PORTB(1)|SIM_SCGC5_PORTC(1)|SIM_SCGC5_PORTD(1);
+
+	//set the corresponding port Pin Control Register to configure the MUX field so that the pin is configured GPIO
+	PORTA->PCR[1] = PORT_PCR_MUX(001);//set PTA1 to ALT1 (Red LED)
+	PORTA->PCR[2] = PORT_PCR_MUX(001);//set PTA2 to ALT1 (Green LED)
+	PORTD->PCR[5] = PORT_PCR_MUX(001);//set PTD5 to ALT1 (Blue LED)
+	PORTC->PCR[1]= PORT_PCR_MUX(001);//set PTC1 to ALT1 (SW2)
+	PORTB->PCR[17] = PORT_PCR_MUX(001);//set PTB17 to ALT1 (SW3)
+	GPIOB->PDDR=0x0000;//set "port data direction register" to input (SW3)
+	GPIOC->PDDR=0x0000;//set "port data direction register" to input (SW2)
+	GPIOA->PDDR=0x0006;//set LED at ports A1 and A2 to output(Red & Green LEDs)
+	GPIOD->PDDR=0x0020;//set led at port D5 to output(Blue LED)
+
+	//int myVariableThatIsNeverUsed = 0;
+	int p = 0;
+	while(1){
+		//Main loop goes here
+		//may need to implement a software debounce to realize reliable pushbutton operation 
+		
+		/*if (SW2&&p!=2)
+			{
+				scroll++;
+				p = 2;
+			}		
+			if (SW3&&p!=3)
+			{
+				scroll--;
+				p = 3;
+			}
+			if (!SW2&&!SW3)
+			{
+			 p = 0;
+			}
+			if (scroll>4)
+			{
+				scroll=0;
+			}	*/
+		
+		//Step 1: Update the integrator based on the input signal.  Note that the
+		//integrator follows the input, decreasing or increasing towards the limits as
+		//determined by the input state (0 or 1). 
+	 
+		if (!SW2)
+		{
+			if (integrator1 > 0)
+			{
+				integrator1--;
+			}
+		}
+		else if (SW2 && integrator1 < MAXIMUM)
+		{
+			integrator1++;
+		}
+		
+		if (!SW3)
+		{
+			if (integrator2 > 0)
+			{
+				integrator2--;
+			}
+		}
+		else if (SW3 && integrator2 < MAXIMUM)
+		{
+			integrator2++;
+		}
+	 
+		//Step 2: Update the output state based on the integrator.  Note that the
+		//output will only change states if the integrator has reached a limit, either
+		//0 or MAXIMUM.
+	 
+		if (integrator1 >= MAXIMUM && scroll > 4)
+		{
+			scroll = 0;
+		}
+		else if (integrator1 >= MAXIMUM && p != 2)
+		{
+			scroll++;
+			integrator1 = MAXIMUM - 25;  //defensive code if integrator got corrupted
+			p = 2;
+		}
+		if (integrator2 >= MAXIMUM && scroll > 0 && p != 3)
+		{
+			scroll--;
+			integrator2 = MAXIMUM - 25;  //defensive code if integrator got corrupted
+			p = 3;
+		}
+		if( integrator1 == 0 && integrator2 == 0)
+		{
+			p = 0;
+		}
+		
+	}
+}
+
---
+++ hw7/hw7.m
@@ -0,0 +1,76 @@
+%% DSP Homework 7
+T = 1/20000;
+Omegap = 2*pi*160*T;    Omegas = 2*pi*40*T; 
+deltap = 1;             deltas = .01;
+Lh = 51; 
+K = 10*Lh; 
+k = (0:K-1);
+Omegak = k*2*pi/K;
+
+OmegaPC = 2*pi*1500*T;   OmegaPR = 2*pi*3000*T;
+parablength = Omegak.*(Omegak>=Omegap & Omegak<=OmegaPR);
+
+sawPer = 1000*2*pi/20000;
+xsawPer= Omegak.*(Omegak > OmegaPR);
+
+sinfn = (.7943-.707)*sawtooth(1/sawPer*xsawPer) + .7;
+% sinfn = ((xsinPer - OmegaPC).^2).*sawtooth(1/sinPer*xsinPer) + .7943;
+plot(xsawPer, sinfn);
+
+
+figure;
+xsauce = [Omegap OmegaPC OmegaPR]; ysauce = [.7943 .8915 .7943]; psauce = polyfit(xsauce,ysauce,2);
+xu=linspace(Omegap,OmegaPR,10);
+parabola = [polyval(psauce,parablength)];
+plot(parablength, parabola)
+
+% Q = 1000.0*(Omegak<=Omegas)+1.0*(Omegak>=Omegap);
+% Q(fix(K/2)+2:end) = Q(round(K/2):-1:2);
+Q = 50.0*(Omegak<=Omegas)++ 1.0*(Omegak>=Omegap & Omegak < 1) + ...
+    .01*(Omegak>=1 & Omegak < 1.5) + 1.0*(Omegak>=1.5);
+Q(fix(K/2)+2:end) = Q(round(K/2):-1:2);
+
+
+Hd = (0.*(Omegak<Omegap) + parabola.*(Omegak>=Omegap & Omegak<=OmegaPR) ...
+     + sinfn.*(Omegak >= OmegaPR)).*exp(-1j*k*pi*(Lh-1)/K);
+Hd(fix(K/2)+2:end) = conj(Hd(round(K/2):-1:2));
+l = (0:Lh-1)'; a = exp(1j*l*Omegak)*Q.'/K; b = exp(1j*l*Omegak)*(Hd.*Q/K).';
+a = real(a); b = real(b);
+A = toeplitz(a); h = (A\b);
+n = (0:Lh-1)'; 
+subplot(211); 
+stem(n,h);
+Omega = linspace(0,pi,1001); 
+H = polyval(h,exp(1j*Omega)).*exp(-1j*(Lh-1)*Omega);
+subplot(212); 
+plot(Omega,20*log10(abs(H)))
+deltap = 2*max(abs(abs(H(Omega>=Omegap))-1))
+deltas = max(abs(H(Omega<=Omegas)))
+
+figure;
+plot(Q);
+
+Morder = length(h);
+hMax = max(h)
+scale = 2^(16-1)/hMax
+% scale = 
+hInt = floor(h*(scale));
+
+subplot(211); 
+stem(n,hInt);
+Omega = linspace(0,pi,1001); 
+H = polyval(hInt,exp(1j*Omega)).*exp(-1j*(Lh-1)*Omega);
+subplot(212); 
+plot(Omega,20*log10(abs(H)))
+deltap = 2*max(abs(abs(H(Omega>=Omegap))-1))
+deltas = max(abs(H(Omega<=Omegas)))
+
+        fid = fopen('K22_Project_Framework/coef.h','w');
+        fprintf(fid,'#define Korder %i \n',uint8(Morder));
+        fprintf(fid,'int h[Korder][1] = { \n');
+        for Korder = 1:Morder
+            fprintf(fid,'{%i} ,\n', h(Korder,1));
+        end
+        fclose(fid);
+
+save('bpg.mat','hInt');
